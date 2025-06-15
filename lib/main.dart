7bb7464a90b3e2e0f48e1672bbcb0401e499c477
nada630 <nadaashrafabdelohab@gmail.com>
2025-06-15 11:53:55 -0700

    Refactor code structure for improved readability and maintainability

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(BirthDayCardApp());
+}
+
+class BirthDayCardApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xFFD2BCD5),
+        body: Center(
+          child: Image(image: AssetImage("lib/image/birthday_card.png")),
+        ),
+      ),
+    );
+  }
+}
